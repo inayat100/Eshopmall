a566b293d4e9020cb85f48c9327ef7c0c2546589
inayat100 <inayat844127@gmail.com>
2022-07-31 12:54:30 +0100

    s3 bukket

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -90,9 +90,9 @@ DATABASES = {
 }
 
 
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -127,11 +127,6 @@ CACHES = {
 }
 
 
-# API_KEY = "d1f8caeda60ed3b5a6c89faba370984d"
-
-# AUTH_TOKEN = "7cd9964570f4ec2d963fc7664b8fa1d5"
-
-# SALT = "ba42498428154beca785aa905ca2131c"
 
 
 
@@ -156,13 +151,13 @@ MESSAGE_TAGS={
     messages.SUCCESS: 'mgreen'
 }
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfile'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'static')
-]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = 'staticfile'
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR, 'static')
+# ]
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -171,3 +166,31 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static'),]
+
+# AWS Bucket integration
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'eshopmall'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_DEFAULT_ACL = config('AWS_DEFAULT_ACL')
+# END AWS BUCKET 
+
+#this just for test for static file AND MEDIA FILE 
+
+AWS_LOCATION = 'static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'Eshop.storages.MediaStore' #MAKE A FILE SETTING.PY  AND LINK HERE
+#endtest
+
+
+
+# qEcm4J5ls61F2iMHqr9nSvojz1tzcyFPfxDxSaHZ  SKEY
+
+# AKIAZPGJISBQAQ5Z2FO7 ACKEY
